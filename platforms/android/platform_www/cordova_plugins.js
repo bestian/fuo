521d429f0a0478b8b67c28e79831c49207d75129
Bestian Tang <bestian@gmail.com>
2022-08-02 07:58:38 +0800

    small

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
-      "id": "cordova-plugin-firebase-analytics.FirebaseAnalytics",
-      "file": "plugins/cordova-plugin-firebase-analytics/www/FirebaseAnalytics.js",
-      "pluginId": "cordova-plugin-firebase-analytics",
-      "merges": [
-        "cordova.plugins.firebase.analytics"
-      ]
-    },
-    {
       "id": "cordova-plugin-device.device",
       "file": "plugins/cordova-plugin-device/www/device.js",
       "pluginId": "cordova-plugin-device",
@@ -31,23 +23,10 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "merges": [
         "navigator.notification"
       ]
-    },
-    {
-      "id": "cordova-plugin-inappbrowser.inappbrowser",
-      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-      "pluginId": "cordova-plugin-inappbrowser",
-      "clobbers": [
-        "cordova.InAppBrowser.open"
-      ]
     }
   ];
   module.exports.metadata = {
-    "cordova-support-android-plugin": "1.0.2",
-    "cordova-plugin-firebase-analytics": "6.0.1",
-    "cordova-plugin-firebase-inappmessaging": "5.0.0",
     "cordova-plugin-device": "2.1.1-dev",
-    "cordova-plugin-dialogs": "3.0.0-dev",
-    "cordova-plugin-inappbrowser": "5.0.1-dev",
-    "cordova-plugin-wkwebview-engine": "1.2.2"
+    "cordova-plugin-dialogs": "3.0.0-dev"
   };
 });
