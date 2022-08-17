5ffcac4257e2fb7e5ade2d729024bca761945d2c
Bestian Tang <bestian@gmail.com>
2022-08-17 08:43:54 +0800

    v2.0

--- config/index.js
+++ config/index.js
@@ -44,8 +44,8 @@ module.exports = {
   },
 
   build: {
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../www/index.html'),
+    assetsRoot: path.resolve(__dirname, '../www'),
     assetsSubDirectory: 'static',
     assetsPublicPath: ''
   }
